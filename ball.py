1cde95ee2c7ebbd09175d6a986be717fb6c7e9c6
Nattanant <nattanant.p@ku.th>
2024-12-06 10:06:49 +0700

    Last Commit: Sending The Exam

--- ball.py
+++ ball.py
@@ -1,30 +1,55 @@
 import turtle
 
-def draw_ball(color, size, x, y):
-    # draw a circle of radius equals to size at x, y coordinates and paint it with color
-    turtle.penup()
-    turtle.color(color)
-    turtle.fillcolor(color)
-    turtle.goto(x,y-size)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.circle(size)
-    turtle.end_fill()
+class Ball:
+    def __init__(self, color, size, x, y):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
 
-def move_ball(i, xpos, ypos, vx, vy, dt):
-    # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
-    xpos[i] += vx[i]*dt
-    ypos[i] += vy[i]*dt
+    def draw_ball(self, color, size, x, y):
+        # draw a circle of radius equals to size at x, y coordinates and paint it with color
+        turtle.penup()
+        turtle.color(color)
+        turtle.fillcolor(color)
+        turtle.goto(x, y - size)
+        turtle.pendown()
+        turtle.begin_fill()
+        turtle.circle(size)
+        turtle.end_fill()
 
 
-def update_ball_velocity(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
-    # if the ball hits the side walls, reverse the vx velocity
-    if abs(xpos[i]) > (canvas_width - ball_radius):
-        vx[i] = -vx[i]
+    def move_ball(self,i, xpos, ypos, vx, vy, dt):
+        # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
+        xpos[i] += vx[i]*dt
+        ypos[i] += vy[i]*dt
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+
+
+    def update_ball_velocity(self,i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
+        # if the ball hits the side walls, reverse the vx velocity
+        if abs(xpos[i]) > (canvas_width - ball_radius):
+            vx[i] = -vx[i]
+            if self.vy > 0:
+                return (self.canvas_height - self.y - self.size) / self.vy
+            elif self.vy < 0:
+                return (self.canvas_height + self.y - self.size) / (-self.vy)
+            else:
+                return math.inf
 
     # if the ball hits the ceiling or the floor, reverse the vy velocity
-    if abs(ypos[i]) > (canvas_height - ball_radius):
-        vy[i] = -vy[i]
+        if abs(ypos[i]) > (canvas_height - ball_radius):
+            vy[i] = -vy[i]
+        if self.vx > 0:
+            return (self.canvas_width - self.x - self.size) / self.vx
+        elif self.vx < 0:
+            return (self.canvas_width + self.x - self.size) / (-self.vx)
+        else:
+            return math.inf
+
 
 
 
